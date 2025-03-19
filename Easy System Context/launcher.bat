7b13a57c428ed8ba0c31739ccbd9a6325e2fe423
Md Asif <87461866+MdAsifInIT@users.noreply.github.com>
2025-03-20 02:06:47 +0530

    modified:   Easy System Context/launcher.bat

--- Easy System Context/launcher.bat
+++ Easy System Context/launcher.bat
@@ -1,5 +1,5 @@
 @echo off
-:: Check for administrator privileges
+
 net session >nul 2>&1
 if %errorLevel% neq 0 (
   echo Requesting administrative privileges...
@@ -7,33 +7,27 @@ if %errorLevel% neq 0 (
   exit /b
 )
 
-:: Change directory to the script's location
 cd /d "%~dp0"
 
-:: Check if psexec.exe is present
 if not exist "psexec.exe" (
   echo psexec.exe not found. Please ensure it is in the same directory as this script.
   pause
   exit /b
 )
 
-:: Present user with options
 echo.
 echo Select the mode for running PSADT:
-echo 1. interactive (-is cmd)
+echo 1. Interactive (-i 1 -s cmd)
 echo 2. Silent (-s cmd)
 set /p mode="Enter your choice (1 or 2): "
 
-:: Validate user input and run the corresponding PSADT command
 if "%mode%"=="1" (
-  echo Launching PSADT in interactive Mode...
-  start "" psexec.exe -i -s cmd.exe
-  timeout /t 1 /nobreak >nul
-  exit
+  echo Launching PSADT in Interactive Mode...
+  psexec.exe -d -i -s cmd.exe >nul 2>&1
+  exit /b
 ) else if "%mode%"=="2" (
   echo Launching PSADT in Silent Mode...
   start "" psexec.exe -s cmd.exe
-  timeout /t 1 /nobreak >nul
   exit
 ) else (
   echo Invalid option. Please run the script again and select a valid option.
@@ -41,5 +35,4 @@ if "%mode%"=="1" (
   exit /b
 )
 
-:: Close the original window
 exit
